9f131a6b2e193bc783361b5717a43fdd6e64cb08
Max-Rex-2006 <blacklynx2006@gmail.com>
2025-09-03 19:22:29 +0530

    Complete a Percentage Calculation Program

--- PercentageCalc.java
+++ PercentageCalc.java
@@ -1,7 +1,19 @@
 package ARC.Gitdemo.Java;
 
+import java.util.Scanner;
+
 public class PercentageCalc {
     public static void main(String[] args) {
-        
+        float a,b,c,d,e,result;
+        Scanner scn = new Scanner (System.in);
+        System.out.println("Please enter the marks of the five subjects whose percentage you wwant to calculate:");
+        a = scn.nextFloat();
+        b = scn.nextFloat();
+        c = scn.nextFloat();
+        d = scn.nextFloat();
+        e = scn.nextFloat();
+        result = (a+b+c+d+e)/5;
+        scn.close();
+        System.out.println("Your net percentage is:" + result);
     }    
 }
