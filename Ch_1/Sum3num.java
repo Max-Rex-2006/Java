cedbbf36da8d9aa91002ebb200bfbbdc0171e800
Max-Rex-2006 <blacklynx2006@gmail.com>
2025-09-07 11:26:06 +0530

    Add a new Chapter

---
+++ Ch_1/Sum3num.java
@@ -0,0 +1,8 @@
+package ARC.Gitdemo.Java.Ch_1;
+
+public class Sum3num {
+    public static void main(String[] args) {
+        int a = 12, b = 7, c = 21;
+        System.out.println("Sum is: "+a+b+c);
+    }
+}
