b58408ebd056268ef9e4e85b286bbd15ac7817b8
Max-Rex-2006 <blacklynx2006@gmail.com>
2025-09-07 11:34:02 +0530

    Add a new Answer

--- Ch_1/Sum3num.java
+++ Ch_1/Sum3num.java
@@ -1,6 +1,6 @@
 package ARC.Gitdemo.Java.Ch_1;
 
-public class Sum3num {
+public class Sum3Num {
     public static void main(String[] args) {
         int a = 12, b = 7, c = 21;
         System.out.println("Sum is: "+a+b+c);
