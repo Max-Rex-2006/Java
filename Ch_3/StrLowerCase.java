0eb3ffdd6c5e0c57b4f7cae8fe841f63c361083d
Max-Rex-2006 <blacklynx2006@gmail.com>
2025-09-08 20:13:52 +0530

    Add a new program

---
+++ Ch_3/StrLowerCase.java
@@ -0,0 +1,8 @@
+package ARC.Gitdemo.Java.Ch_3;
+
+public class StrLowerCase {
+    public static void main(String[] args) {
+        String name = "Max Rex";
+        System.out.println(name.toLowerCase());
+    }
+}
