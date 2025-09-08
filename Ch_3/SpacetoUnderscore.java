1800c0a44e0c09265df75c522d3cb988ec0cf976
Max-Rex-2006 <blacklynx2006@gmail.com>
2025-09-08 20:24:15 +0530

    Add two new programs

---
+++ Ch_3/SpacetoUnderscore.java
@@ -0,0 +1,8 @@
+package ARC.Gitdemo.Java.Ch_3;
+
+public class SpacetoUnderscore {
+    public static void main(String[] args) {
+        String name = "Max Rex";
+        System.out.println(name.replace(" ","_"));
+    }
+}
