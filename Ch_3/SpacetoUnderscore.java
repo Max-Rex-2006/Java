373bb0b4034472bb224c14b91e9d50f4d82914ac
Max Rex <trex.eternatus@gmail.com>
2025-09-11 18:13:26 +0530

    Delete Ch_3 directory

--- Ch_3/SpacetoUnderscore.java
+++
@@ -1,8 +0,0 @@
-package ARC.Gitdemo.Java.Ch_3;
-
-public class SpacetoUnderscore {
-    public static void main(String[] args) {
-        String name = "Max Rex";
-        System.out.println(name.replace(" ","_"));
-    }
-}
--- Ch_3/StrLowerCase.java
+++
@@ -1,8 +0,0 @@
-package ARC.Gitdemo.Java.Ch_3;
-
-public class StrLowerCase {
-    public static void main(String[] args) {
-        String name = "Max Rex";
-        System.out.println(name.toLowerCase());
-    }
-}
