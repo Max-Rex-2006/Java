9689ac941e1d300bf73e9283e3763d6d47c30eb9
Max Rex <trex.eternatus@gmail.com>
2025-09-11 18:12:38 +0530

    Delete Ch_1 directory

--- Ch_1/CGPACalc.java
+++
@@ -1,9 +0,0 @@
-package ARC.Gitdemo.Java.Ch_1;
-
-public class CGPACalc {
-    public static void main(String[] args) {
-        float sub1 = 79f, sub2 = 92f, sub3 = 72f, result;
-        result = (sub1 + sub2 + sub3)/30;
-        System.out.println("The Net CGPA is: " + result);
-    }
-}
--- Ch_1/Sum3num.java
+++
@@ -1,9 +0,0 @@
-package ARC.Gitdemo.Java.Ch_1;
-
-public class Sum3Num {
-    public static void main(String[] args) {
-        int a = 12, b = 7, c = 21, sum;
-        sum = a+b+c;
-        System.out.println("Sum is: "+sum);
-    }
-}
